356983952cf69767461d7cf836b264536ff5c836
Yihui <maxhyh@outlook.com>
2020-06-05 11:32:45 -0400

    task2_update_order_book

--- section1/task2/task2.py
+++ section1/task2/task2.py
@@ -2,16 +2,30 @@ from binance.client import Client
 from datetime import datetime
 import os
 import pandas as pd
+import numpy as np
 from binance.websockets import BinanceSocketManager
 
 class binance_data():
 
-    def __init__(self):
-        self.client = Client("","")
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+        self.client = Client(api_key,api_secret)
 
-    def get_order_book(self):
+    def get_order_book(self, symbol):
 
-        return self.client.get_orderbook_tickers()
+        order_book = self.client.get_order_book(symbol = symbol)
+
+        bids = pd.DataFrame.from_dict(order_book["bids"]).iloc[:, 0:2]
+        bids.columns = ['bid_price', 'bid_qty']
+
+  
+        asks = pd.DataFrame.from_dict(order_book["asks"]).iloc[:, 0:2]
+        asks.columns = ['ask_price', 'ask_qty']
+        
+        data = pd.concat([asks, bids])
+
+        return data
 
 
     def get_historical_klines(self, symbol, interval, start_str, end_str):
@@ -22,6 +36,15 @@ class binance_data():
             return self.client.get_recent_trades(symbol=symbol)
         else:
             raise Exception("Symbol should be an instance of string.")
+    
+    def test_order(self, symbol, quantity):
+        test_order = self.client.create_test_order(
+            symbol=symbol,
+            side=Client.SIDE_BUY,
+            type=Client.ORDER_TYPE_MARKET,
+            quantity=quantity)
+        return test_order
+
 
 def unix2date(timestamp):
     date = int(timestamp)/1000
@@ -30,12 +53,12 @@ def unix2date(timestamp):
 
 DATA_DIR = '/Users/yihuihuang/Desktop/Crypto/caw-quant-training/section1/task2/'
 
-bd = binance_data()
+bd = binance_data("fF1krtgLynipQf8yOZ1h1iGRGZM2K6ukAelCCsu05AE2gZl4fcQEVBosKXINw3IT", 
+    "NsEoj50t36tTt8sFYcnihGJvmjf648agQHZIgknerDcHdDYmndFlAnihMBbflulj")
 
 
-data_MD = bd.get_order_book()
-df1 = pd.DataFrame(data_MD)
-df1.to_csv(os.path.join(DATA_DIR, "market_depth.csv"), index=False)
+data_MD = bd.get_order_book(symbol="ETHBTC")
+data_MD.to_csv(os.path.join(DATA_DIR, "market_depth.csv"), index=False)
 
 data_his = bd.get_historical_klines("ETHBTC", Client.KLINE_INTERVAL_1WEEK, "1 Apr, 2019", "1 Apr, 2020")
 df2 = pd.DataFrame(data_his, columns=['open_time', 'open', 'high', 'low', 'close', 'volume', 'close_time',\
